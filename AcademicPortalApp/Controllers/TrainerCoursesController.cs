9ff13368f61ebfe911b042d2a13fff893740ea19
nguyenhuan123 <huangcd18611@fpt.edu.vn>
2021-02-09 23:13:15 +0700

    Implement Assign Courses to Trainers

--- AcademicPortalApp/Controllers/TrainerCoursesController.cs
+++ AcademicPortalApp/Controllers/TrainerCoursesController.cs
@@ -30,7 +30,54 @@ namespace AcademicPortalApp.Controllers
         }
 
 
+        //GET: Staff/Assign Course to trainer: return :select trainer and course view model to view 
+        [HttpGet]
+        [Authorize(Roles = "Staff")]
+        public ActionResult AssignCoursesTrainers()
+        {
+            var viewModel = new ViewModelCoursesTrainer()
+            {
+                Trainers = _context.Users.OfType<Trainer>().ToList(),
+                Courses = _context.Courses.ToList()
+            };
 
+            return View(viewModel);
+        }
+        //POST: Staff/ receive trainerid and courseid from viewmodel to create new trainer course and redirect to all trainer course page 
+        [HttpPost]
+        [Authorize(Roles = "Staff")]
+        public ActionResult AssignCoursesTrainers(ViewModelCoursesTrainer model)
+        {
+            var trainerCourse = new TrainerCourses()
+            {
+                TrainerId = model.TrainerId,
+                CourseId = model.CourseId
+            };
+
+            _context.TrainerCourses.Add(trainerCourse);
+            _context.SaveChanges();
+
+            return RedirectToAction("AllCourseRelatedTrainer", "TrainerCourses", new { trainerId = model.TrainerId });
+        }
+
+
+
+
+
+
+
+     
+
+        [HttpGet]
+        [Authorize(Roles = "Staff")]
+        public ActionResult RemoveTrainerFromCourse(int Id)
+        {
+            var findTrainerCourse = _context.TrainerCourses.SingleOrDefault(t => t.Id == Id);
+            var trainerId = findTrainerCourse.TrainerId;
+            _context.TrainerCourses.Remove(findTrainerCourse);
+            _context.SaveChanges();
+            return RedirectToAction("AllCourseRelatedTrainer", "TrainerCourses", new { trainerId = trainerId });
+        }
 
 
 
@@ -48,7 +95,7 @@ namespace AcademicPortalApp.Controllers
 
 
         // find trainer course by id and assign trainerid for redirect to all trainer course then remove trainer course had been found
-     
+
 
 
     }
