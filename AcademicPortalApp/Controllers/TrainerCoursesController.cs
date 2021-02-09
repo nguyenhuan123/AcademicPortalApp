cb1e4248aec945a8a594b33748a8859a46853356
nguyenhuan123 <huangcd18611@fpt.edu.vn>
2021-02-09 23:22:23 +0700

    Implement Staff : Reassign Courses to Trainers

--- AcademicPortalApp/Controllers/TrainerCoursesController.cs
+++ AcademicPortalApp/Controllers/TrainerCoursesController.cs
@@ -61,6 +61,33 @@ namespace AcademicPortalApp.Controllers
         }
 
 
+        //GET: Staff/ find trainer course by id and trainer id and create a new view model of trainer course to return trainer course, list of course and trainer id
+        [HttpGet]
+        [Authorize(Roles = "Staff")]
+        public ActionResult ReassignedTrainerCourse(int Id)
+        {
+            var trainerCourse = _context.TrainerCourses.SingleOrDefault(t => t.Id == Id);
+            var trainerId = trainerCourse.TrainerId;
+            ViewModelCoursesTrainer model = new ViewModelCoursesTrainer
+            {
+                TrainerCourse = trainerCourse,
+                Courses = _context.Courses.ToList(),
+                TrainerId = trainerId
+            };
+
+            return View(model);
+        }
+        //POST: Staff/ find trainer course by trainer course by id and change courseid that receive from view model
+        [HttpPost]
+        [Authorize(Roles = "Staff")]
+        public ActionResult ReassignedTrainerCourse(ViewModelCoursesTrainer model)
+        {
+            var trainerCourse = _context.TrainerCourses.SingleOrDefault(t => t.Id == model.TrainerCourse.Id);
+            trainerCourse.CourseId = model.TrainerCourse.CourseId;
+            _context.SaveChanges();
+
+           return RedirectToAction("AllCourseRelatedTrainer", "TrainerCourses", new { trainerId = model.TrainerId });
+        }
 
 
 
